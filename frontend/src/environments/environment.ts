02d20566295eaaf3ad0d78818463d95683b7419f
fodela <particleme30@gmail.com>
2022-07-09 18:00:25 +0000

    feat: set up front environment variables

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,12 +3,12 @@
  */
 
 export const environment = {
-  production: false,
-  apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
-  auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
-  }
+	production: false,
+	apiServerUrl: "http://127.0.0.1:5000", // the running FLASK api server url
+	auth0: {
+		url: "dev-kro3nq5t.us", // the auth0 domain prefix
+		audience: "coffee_shop", // the audience set for the auth0 app
+		clientId: "VkjYk43DXJ5WYMzglihR5GwOZyNMiORB", // the client id generated for the auth0 app
+		callbackURL: "http://localhost:8100", // the base url of the running ionic application.
+	},
 };
